dcd7b4665ca3659dd4cdc32d4c8bfb2c2ea73ce5
Alvaro <alvaro.barba.lopez@gmail.com>
2023-05-30 01:34:08 +0200

    update

---
+++ src/app/forum/forum.js
@@ -0,0 +1,43 @@
+forum.js
+//Estoy seguro que hay una manera de cambiar el fondo de color con menos codigo usando getElementsByTagName,
+//así que estoy haciendo algunas busquedas por internet, de momento lo dejo funcionando con ById.
+function dentro(){
+    document.getElementById('primero').style.backgroundColor = '#2778d3';
+    document.getElementById('autor1').style.color = 'white';
+    document.getElementById('tema1').style.color = 'white';
+}
+function fuera(){
+    document.getElementById('primero').style.backgroundColor = 'white';
+    document.getElementById('autor1').style.color = '#2778d3';
+    document.getElementById('tema1').style.color = '#2778d3';
+}
+function dentro2(){
+    document.getElementById('segundo').style.backgroundColor = '#2778d3';
+    document.getElementById('autor2').style.color = 'white';
+    document.getElementById('tema2').style.color = 'white';
+}
+function fuera2(){
+    document.getElementById('segundo').style.backgroundColor = 'white';
+    document.getElementById('autor2').style.color = '#2778d3';
+    document.getElementById('tema2').style.color = '#2778d3';
+}
+function dentro3(){
+    document.getElementById('tercero').style.backgroundColor = '#2778d3';
+    document.getElementById('autor3').style.color = 'white';
+    document.getElementById('tema3').style.color = 'white';
+}
+function fuera3(){
+    document.getElementById('tercero').style.backgroundColor = 'white';
+    document.getElementById('autor3').style.color = '#2778d3';
+    document.getElementById('tema3').style.color = '#2778d3';
+}
+function dentro4(){
+    document.getElementById('cuarto').style.backgroundColor = '#2778d3';
+    document.getElementById('autor4').style.color = 'white';
+    document.getElementById('tema4').style.color = 'white';
+}
+function fuera4(){
+    document.getElementById('cuarto').style.backgroundColor = 'white';
+    document.getElementById('autor4').style.color = '#2778d3';
+    document.getElementById('tema4').style.color = '#2778d3';
+}
