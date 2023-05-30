487a4b8140a3029fa4d6c0f1760bc623ee5d2afd
AlvaroBaLo <123897181+AlvaroBaLo@users.noreply.github.com>
2023-05-31 00:19:40 +0200

    Update forum.component.ts

--- src/app/forum/forum.component.ts
+++ src/app/forum/forum.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-forum',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./forum.component.css']
 })
 export class ForumComponent {
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
 
+  ngMouseOver(elementId: string) {
+    const element = this.elementRef.nativeElement.querySelector(`#${elementId}`);
+    this.renderer.setStyle(element, 'background-color', '#9988d3');
+    this.renderer.setStyle(element, 'color', 'white');
+  }
+
+  ngMouseOut(elementId: string) {
+    const element = this.elementRef.nativeElement.querySelector(`#${elementId}`);
+    this.renderer.setStyle(element, 'background-color', 'white');
+    this.renderer.setStyle(element, 'color', 'black');
+  }
 }
